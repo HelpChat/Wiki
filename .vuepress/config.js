077ba68a93fe45d0d32685c1e1d2db006577cb9d
darbyjack <admin@glaremasters.me>
2019-05-24 08:30:13 -0500

    Added in Nav Links

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,5 +3,15 @@ module.exports = {
   description: 'Coming Soon... Your one-stop-shop for information about hundreads of plugins and answers to lots of common questions plus a lot more stuff!',
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }]
-  ]
+  ],
+  themeConfig: {
+    logo: '/logo.png',
+    nav: [
+      { text: 'Home', link: '#' },
+      { text: 'Paste', link: 'https://paste.helpch.at/' },
+      { text: 'Yaml', link: 'https://yaml.helpch.at/' },
+      { text: 'Discord', link: 'https://helpch.at/discord' },
+      { text: 'Docs', link: 'https://helpch.at/docs' },
+    ],
+  }
 }
