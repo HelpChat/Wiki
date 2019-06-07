edcd5676e0abf18b664192ee32240b14c5ffd4ff
darbyjack <admin@glaremasters.me>
2019-06-07 10:37:56 -0500

    Testing Auto Deploy

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd .vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:help-chat/Wiki.git master:gh-pages
+
+cd -
